8c9fa6d1a7f75582976af8623d7006978edb8f78
Weihua Liu <auhiewuil@gmail.com>
2020-11-24 21:53:19 +0800

    leetcode 1389

---
+++ rust/src/leetcode1389.rs
@@ -0,0 +1,38 @@
+
+struct Solution();
+
+impl Solution {
+    pub fn create_target_array(nums: Vec<i32>, index: Vec<i32>) -> Vec<i32> {
+        let mut v = Vec::new();
+        // init default value -1
+        for i in 0..nums.len() {
+            v.push(-1);
+        }
+
+        for i in 0..nums.len() {
+            let j: usize = index[i] as usize;
+            if *v.get(j as usize).unwrap() == -1 {
+                v[j] = nums[i as usize];
+            }else {
+                v.insert(j, nums[i as usize]);
+            }
+        }
+
+        v.truncate(nums.len());
+        v
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::leetcode1389::Solution;
+
+    #[test]
+    fn t_create_target_array() {
+        let nums = vec![0,1,2,3,4];
+        let index = vec![0,1,2,2,1];
+        let result = vec![0,4,1,3,2];
+        assert_eq!(Solution::create_target_array(nums, index), result);
+    }
+
+}
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,3 +1,4 @@
 mod leetcode1313;
 mod leetcode1480;
 mod leetcode1365;
+mod leetcode1389;
